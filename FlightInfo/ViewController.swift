8972ea1370062bfb75bdfd92ace1692865d9de39
tanaka <tanaka.mawoyo@yahoo.com>
2022-09-19 10:37:25 +0200

    Initial Commit

---
+++ FlightInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlightInfo
+//
+//  Created by Tanaka Mawoyo on 19.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
