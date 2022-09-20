d5c5c88d36c772bfefbbeccfd49c9c4a3dc9c57e
tanaka <tanaka.mawoyo@yahoo.com>
2022-09-20 19:01:59 +0200

    Networking implemented

--- FlightInfo/FlightManager.swift
+++ FlightInfo/FlightManager.swift
@@ -9,10 +9,41 @@ import Foundation
 
 struct FlightManager {
     
-    let flightURL = "http://api.aviationstack.com/v1/flights?access_key=b3ca408d77b4ef372c168d706194734b"
+    let flightURL = "https://airlabs.co/api/v9/flight?api_key=5eb680f1-5edd-4228-ae86-6b7a521f3a45"
     
     func fetchFlight(flightNumber: String){
         let urlString = "\(flightURL)&flight_iata=\(flightNumber)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    //MARK: - Networking
+    func performRequest(urlString: String){
+        //1. Create a URL
+        
+        if let url = URL(string: urlString){
+            
+            //2. Create a UrlSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give URLSession a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            //4. Start the task
+            task.resume()
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void{
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+    }
+    
 }
